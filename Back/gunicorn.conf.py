62ec9c3d625ae762832e5219fcbbb8c665a0a3f3
jcarm98 <32147196+jcarm98@users.noreply.github.com>
2022-11-09 16:52:09 -0800

    Update gunicorn.conf.py

--- Back/gunicorn.conf.py
+++ Back/gunicorn.conf.py
@@ -3,7 +3,7 @@ bind = "127.0.0.1:8001"
 workers = 3
 
 if os.getenv("DJANGO_ENV") == "live":
-    certfile = '/etc/letsencrypt/live/hobbyshare.app/fullchain.pem'
-    keyfile = '/etc/letsencrypt/live/hobbyshare.app/privkey.pem'
+    certfile = f'/etc/letsencrypt/live/{os.getenv("DJANGO_HOST")}/fullchain.pem'
+    keyfile = f'/etc/letsencrypt/live/{os.getenv("DJANGO_HOST")}/privkey.pem'
 
 log_file = '-'
